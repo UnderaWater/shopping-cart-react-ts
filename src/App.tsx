8ecc15a624d536c777d6cd6911e5544d4ba20dab
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-03-30 19:49:22 +0300

    add: added item component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { Wrapper } from './App.styles';
 import { CartItemType } from './types/CartItem';
 import { getProducts } from './api/api';
+import Item from './components/item/Item';
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItem, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
 
   const getTotalItems = () => null;
@@ -15,13 +18,22 @@ const App = () => {
 
   const handleRemoveFromCart = () => null;
 
-  if (isLoading) return <LinearProgress />
-  if (error) return <div>Something went wrong...</div>
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
 
   return (
-    <div className="App">
-
-    </div>
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart
+      </Drawer>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,16 +1,32 @@
+import { Button } from '@material-ui/core';
 import React from 'react'
 import { Wrapper } from '../../App.styles';
 import { CartItemType } from '../../types/CartItem';
 
 type ItemProps = {
-    item: CartItemType;
+  item: CartItemType;
+  handleAddToCart: (clickedItemt: CartItemType) => void;
 }
 
-const Item = () => {
+const Item: React.FC<ItemProps> = ({ item, handleAddToCart }) => {
   return (
-    <div>
-        Item
-    </div>
+    <Wrapper>
+      <img src={item.image} alt={item.title} />
+      <div>
+        <h3>
+          {item.title}
+        </h3>
+        <p>
+          {item.description}
+        </p>
+        <h3>
+          ${item.price}
+        </h3>
+      </div>
+      <Button onClick={() => handleAddToCart(item)}>
+        Add to cart
+      </Button>
+    </Wrapper>
   )
 }
 
